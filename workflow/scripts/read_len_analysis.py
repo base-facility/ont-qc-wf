7117d6ecb4f96eaffe58c7509aab7655a7e5e662
moe.z <mzed@pm.me>
2025-11-14 14:40:39 +1000

    read len script updates

--- workflow/scripts/read_len_analysis.py
+++ workflow/scripts/read_len_analysis.py
@@ -4,12 +4,14 @@
 # Get read length distribution
 # Input: Mapped bam file
 # Output: Read length distribution histogram plots
+# Author: Moe
 
-import pysam
+import pysam, json
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from bioUtils import seqUtils
+import statistics
 
 infile = sys.argv[1]
 outprefix = sys.argv[2]
@@ -17,29 +19,51 @@ sample = sys.argv[3]
 
 def plot_read_length_histogram(bam_file, output_prefix, sample_name):
     with pysam.AlignmentFile(bam_file, 'rb') as bam:
-        # read_lengths = [read.query_length for read in bam if read.is_mapped]
+        discarded_ids = []
+        alignment_spans = []
         read_length = []
+        # filter primary mapped reads only
         for read in bam:
-            if read.is_mapped  and not read.is_supplementary and not read.is_duplicate and not read.is_secondary and read.mapping_quality == 60:
+            if (not read.query_sequence or read.is_supplementary or read.is_duplicate or read.is_secondary or read.is_qcfail or read.is_unmapped):
+                discarded_ids.append(read.query_name)
+                continue
+            if read.is_mapped:
                 query = read.query_sequence
-                query_id = read.query_name
-                # Debug
-                if not query:
-                    print(query_id)
+
+                # alignment span as length
+                span_len = read.reference_end - read.reference_start + 1
+                alignment_spans.append(span_len)
+                
                 # Without removing polyA
                 # read_length.append(len(query))
+                
                 # With polyA removed
                 seq_obj = seqUtils(query)
                 polished_query = seq_obj.rmPolyA()
                 read_length.append(len(polished_query))
+            else:
+                print("*** ERROR ***")
+                print(read.query_name)
+                break
+
+        json.dump(discarded_ids, open(f'{output_prefix}/{sample_name}_discarded_ids.json', 'w'), indent=4)
+        json.dump(alignment_spans, open(f'{output_prefix}/{sample_name}_align_spans.json', 'w'), indent=4)
+        json.dump(read_length, open(f'{output_prefix}/{sample_name}_rl.json', 'w'), indent=4)
 
 
     # Plot histogram of read lengths
     plt.figure(figsize=(10, 6))
-    xlim = 2000
+    # set xlim to 99th percentile to remove very long concatermer from plot
+    xlim = np.percentile(read_length, 99)
+    # clipped = np.clip(read_length, None, xlim)
     bins = np.arange(0, xlim + 10, 10)
+    median_rl = np.median(read_length)
+    mean_rl = np.mean(read_length)
+    plt.axvline(median_rl, color='red', linestyle='--', label=f"Median: {median_rl:.0f}")
+    plt.axvline(mean_rl, color='green', linestyle='-', label=f"Mean: {mean_rl:.0f}")
+
     plt.hist(read_length, bins=bins, color='skyblue', edgecolor='black')
-    plt.xlim(0, xlim)
+    plt.xlim(0, xlim + 10)
     plt.title(f'{sample_name} - Distribution of Read Lengths')
     plt.xlabel('Read Length (bp)')
     plt.ylabel('Frequency')
@@ -49,11 +73,21 @@ def plot_read_length_histogram(bam_file, output_prefix, sample_name):
     plt.savefig(f"{output_prefix}/{sample_name}_rl_hist.png", format='png', dpi=300)  # Rasterized format (PNG)
     plt.savefig(f"{output_prefix}/{sample_name}_rl_hist.pdf", format='pdf')  # Vector format (PDF)
 
-plot_read_length_histogram(infile, outprefix, sample)
+    plt.close()
+    
+    # Plot histogram of read spans
+    plt.figure(figsize=(10, 6))
+    bins = np.arange(0, max(alignment_spans) + 10, 10)
+    plt.hist(alignment_spans, bins=bins, color='skyblue', edgecolor='black')
+    plt.title(f'{sample_name} - Distribution of alignment lengths')
+    plt.xlabel('Read length (bp)')
+    plt.ylabel('Frequency')
+    
+    # Save the plot in different formats
+    plt.savefig(f"{output_prefix}/{sample_name}_alignment_span_hist.svg", format='svg')  # Vector format (SVG)
+    plt.savefig(f"{output_prefix}/{sample_name}_alignment_span_hist.png", format='png', dpi=300)  # Rasterized format (PNG)
+    plt.savefig(f"{output_prefix}/{sample_name}_alignment_span_hist.pdf", format='pdf')  # Vector format (PDF)
 
-# Dev
-# read.get_tag("qs") >= 10
-# read.mapping_quality == 60
-# seq_obj = seqUtils(query)
-# polished_query = seq_obj.rmPolyA()
-# read_length.append(len(polished_query))
+    plt.close()
+
+plot_read_length_histogram(infile, outprefix, sample)
